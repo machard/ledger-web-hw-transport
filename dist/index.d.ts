6f9508035243f30a418235eefae58b0ecba0bc51
machard <matthieu__@hotmail.fr>
2021-07-02 20:12:56 +0200

    prepare

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import Transport from '@ledgerhq/hw-transport';
-import Client from 'ledger-web-client';
-export default class TransportLedgerWeb extends Transport {
-    client: Client;
-    constructor(client: Client);
-    send: (cla: number, ins: number, p1: number, p2: number, _data?: Buffer, _statusList?: number[]) => Promise<Buffer>;
-    setScrambleKey(): void;
-}
